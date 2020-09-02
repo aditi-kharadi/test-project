735a757bb6152c8c216f659bd949e74410cae498
aditi-kharadi <60316536+aditi-kharadi@users.noreply.github.com>
2020-09-03 01:20:24 +0530

    Updates

---
+++ precache-manifest.a23d3c2914486f1464be24398398740d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373606cd26a260cc4e9e331fd5fc0b44",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "483cad3bfa41e10e5b0e",
+    "url": "/test-project/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8f99b302851fdb50ba92",
+    "url": "/test-project/static/css/main.632fdeb2.chunk.css"
+  },
+  {
+    "revision": "483cad3bfa41e10e5b0e",
+    "url": "/test-project/static/js/2.74784200.chunk.js"
+  },
+  {
+    "revision": "536558ae0d2c3676e46653cadf22447a",
+    "url": "/test-project/static/js/2.74784200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f99b302851fdb50ba92",
+    "url": "/test-project/static/js/main.979eedcf.chunk.js"
+  },
+  {
+    "revision": "e893b0a89f2d626c5752",
+    "url": "/test-project/static/js/runtime-main.16e8256d.js"
+  },
+  {
+    "revision": "88f0637a82cc4593cf0ce41942f9ac6b",
+    "url": "/test-project/static/media/user.88f0637a.jpg"
+  }
+]);
